1323471143aaaf1460867ba7b2dba16f3a206c01
hayatonakamura <33381712+hayatonakamura@users.noreply.github.com>
2018-02-19 19:42:14 -0500

    Create flasktest.py

---
+++ API/codereview/flasktest.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+first = '<!DOCTYPE html> <html lang="en"> <head> <meta charset = "UTF-8"> <title>Title</title> </head> <body> <h1> EC500 LOCAL WEBSITE: HAYATO NAKAMURA </h1> <p>'
+second = "</p> </body> </html>"
+
+imageurl = "https://pbs.twimg.com/media/DVJ7JaAVMAAESdw.jpg"
+complete = '<img src="' + imageurl + '" width="20%" height="20%">'
+
+run = first + complete + second
+
+@app.route("/")
+def hello():
+	return (run)
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True, use_reloader=True)
+
